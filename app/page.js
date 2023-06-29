352587aa2ab3c4ad08d57cee2177981607fa821d
Julian <julian_delisio@hotmail.com>
2023-06-28 23:04:29 -0300

    formulario

--- app/page.js
+++ app/page.js
@@ -36,12 +36,12 @@ export default function Home() {
           </form>
           <ul>
             {items.map((item, id) => (
-              <li key={id} className="flex justify-between w-full my-4">
+              <li key={id} className="flex justify-between w-full my-4 bg-slate-950">
                 <div className="flex justify-between w-full p-4">
                   <span className="capitalize">{item.name}</span>
                   <span>%{item.price}</span>
                 </div>
-                <button className="p-4 ml-8 border-l-2 border-slate-900 hover:">X</button>
+                <button className="w-16 p-4 ml-8 border-l-2 border-slate-900 hover:bg-slate-900">X</button>
               </li>
             ))}
           </ul>

19a5d0607a70bc469c5046e87dcaff94a848a97c
Julian <julian_delisio@hotmail.com>
2023-07-02 14:26:00 -0300

    formulario terminado

--- app/page.js
+++ app/page.js
@@ -12,42 +12,64 @@ export default function Home() {
   const [total, setTotal] = useState(0);
 
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-4 sm:p-24">
-      <div className="z-10 items-center justify-between w-full font-mono text-sm max-w-5x ">
-        <h1 className="p-4 text-4xl text-center">Expense Tracker</h1>
-        <div className="p-4 rounded-lg bg-slate-800">
-          <form className="grid items-center grid-cols-6 text-black">
-            <input
-              type="text"
-              placeholder="Enter Item"
-              className="col-span-3 p-3 border"
-            />
-            <input
-              type="number"
-              placeholder="Enter $"
-              className="col-span-3 p-3 mx-3 border"
-            />
-            <button
-              type="submit"
-              className='p-3 text-xl text-white bg-slate-950 hover:bg-slate-900'
+    <main className='flex min-h-screen flex-col items-center justify-between p-4'>
+    <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm '>
+      <h1 className='text-4xl p-4 text-center my-4'>Expense Tracker</h1>
+      <div className='bg-slate-800 p-4 rounded-lg text-white'>
+        <form className='grid grid-cols-6 items-center text-black'>
+          <input
+            /*value={newItem.name}
+            onChange={(e) => setNewItem({ ...newItem, name: e.target.value })}*/
+            className='col-span-3 p-3 border'
+            type='text'
+            placeholder='Enter Item'
+          />
+          <input
+            /*value={newItem.price}
+            onChange={(e) =>
+              setNewItem({ ...newItem, price: e.target.value })
+            }*/
+            className='col-span-2 p-3 border mx-3'
+            type='number'
+            placeholder='Enter $'
+          />
+          <button
+            //onClick={addItem}
+            className='text-white bg-slate-950 hover:bg-slate-900 p-3 text-xl'
+            type='submit'
+          >
+            +
+          </button>
+        </form>
+        <ul>
+          {items.map((item, id) => (
+            <li
+              key={id}
+              className='my-4 w-full flex justify-between bg-slate-950'
             >
-              +
-            </button>
-          </form>
-          <ul>
-            {items.map((item, id) => (
-              <li key={id} className="flex justify-between w-full my-4 bg-slate-950">
-                <div className="flex justify-between w-full p-4">
-                  <span className="capitalize">{item.name}</span>
-                  <span>%{item.price}</span>
-                  <span>%{item.price}</span>
-                </div>
-                <button className="w-16 p-4 ml-8 border-l-2 border-slate-900 hover:bg-slate-900">X</button>
-              </li>
-            ))}
-          </ul>
-        </div>
+              <div className='p-4 w-full flex justify-between'>
+                <span className='capitalize'>{item.name}</span>
+                <span>${item.price}</span>
+              </div>
+              <button
+                //onClick={() => deleteItem(item.id)}
+                className='ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16'
+              >
+                X
+              </button>
+            </li>
+          ))}
+        </ul>
+        {items.length < 1 ? (
+          ''
+        ) : (
+          <div className='flex justify-between p-3'>
+            <span>Total</span>
+            <span>${total}</span>
+          </div>
+        )}
       </div>
-    </main>
+    </div>
+  </main>
   );
 }
